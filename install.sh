a3f7360122ef102bacbec34570f0add3f6b24142
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-09-16 13:45:05 +0100

    Adds polybar launcher

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo cp scripts/i3memory /usr/bin/i3memory
+sudo cp scripts/i3wifi /usr/bin/i3wifi
+sudo cp scripts/i3weather /usr/bin/i3weather
+sudo cp scripts/polycpu /usr/bin/polycpu
+sudo cp scripts/polypulsevolume /usr/bin/polypulsevolume
+sudo cp scripts/polytempamd /usr/bin/polytempamd
+sudo cp scripts/updatepackages /usr/bin/updatepackages
+sudo cp scripts/bspfullscreen /usr/bin/bspfullscreen
+sudo cp scripts/bsphide /usr/bin/bsphide
+sudo cp scripts/crystal /usr/bin/crystal
+sudo cp scripts/bspswallow /usr/bin/bspswallow
+sudo cp scripts/bspunhide /usr/bin/bspunhide
+sudo cp scripts/tabc /usr/bin/tabc
+sudo cp scripts/tabdmenu /usr/bin/tabdmenu
+
+
